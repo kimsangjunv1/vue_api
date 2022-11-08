98e9341506ba97a2298b28d5c4a8156173f9a519
kimsangjunv1 <ingyoe64@gmail.com>
2022-11-08 14:47:15 +0900

    “2022.11.08”

---
+++ src/components/ContactCont.vue
@@ -0,0 +1,7 @@
+<template>
+  <section className="cont_contact">
+    <p>Let's create something new</p>
+    <h3>LET'S Get IN <em>TOUCH</em></h3>
+    <a href="mailto:webstoryboy@naver.com">Contact me</a>
+  </section>
+</template>
